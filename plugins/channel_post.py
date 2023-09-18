a09e920e04304460962f6319cc7eb3e4b221dde7
Prajwal52 <134858777+Prajwal52@users.noreply.github.com>
2023-09-18 13:17:46 +0530

    Update channel_post.py

--- plugins/channel_post.py
+++ plugins/channel_post.py
@@ -15,7 +15,7 @@ async def channel_post(client: Client, message: Message):
     try:
         post_message = await message.copy(chat_id = client.db_channel.id, disable_notification=True)
     except FloodWait as e:
-        #await asyncio.sleep(e.x)
+        await asyncio.sleep(e.x)
        
         post_message = await message.copy(chat_id = client.db_channel.id, disable_notification=True)
     except Exception as e:
