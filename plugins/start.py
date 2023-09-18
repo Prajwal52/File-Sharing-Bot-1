2bfd7249739bbe505ba676720fe3f77779073b6c
Prajwal52 <134858777+Prajwal52@users.noreply.github.com>
2023-09-18 12:16:51 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -55,7 +55,7 @@ async def start_command(client: Client, message: Message):
                 ids = [int(int(argument[1]) / abs(client.db_channel.id))]
             except:
                 return
-        temp_msg = await message.reply("Please wait...")
+        temp_msg = await message.reply("Files will be deleted in 30 minutes to avoid copyright issues. Please forward and save them.")
         try:
             messages = await get_messages(client, ids)
         except:
@@ -77,10 +77,11 @@ async def start_command(client: Client, message: Message):
 
             try:
                 await msg.copy(chat_id=message.from_user.id, caption = caption, parse_mode = ParseMode.HTML, reply_markup = reply_markup, protect_content=PROTECT_CONTENT)
-                await asyncio.sleep(0.5)
+                await asyncio.sleep(30)
             except FloodWait as e:
                 await asyncio.sleep(e.x)
                 await msg.copy(chat_id=message.from_user.id, caption = caption, parse_mode = ParseMode.HTML, reply_markup = reply_markup, protect_content=PROTECT_CONTENT)
+                await message.delete(30)
             except:
                 pass
         return
